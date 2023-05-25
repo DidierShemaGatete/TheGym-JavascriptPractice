59d8a211ce376b273fb3c1d1c3846d2aebebf3cd
DidierShemaGatete <didiershemagate01@gmail.com>
2023-05-25 06:44:08 +0300

    javascriptTrials

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// DATA TYPES
+
+// Boolean -True or False
+// Null - No value (typeof = Object)
+// Undefined - defined var with no value
+//Number - 
+//String - Array if characters
+// Symbol - Unique Value
+
+let b = Math.ceil(4.5);
+
+let v = Math.floor(8.4);
+
+
+console.log(b);
+console.log(v);
