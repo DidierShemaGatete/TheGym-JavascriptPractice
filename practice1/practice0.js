49abc898962723453bacf43bdc460985d1c31ef6
DidierShemaGatete <didiershemagate01@gmail.com>
2023-07-01 00:03:01 +0300

    recap

---
+++ practice1/practice0.js
@@ -0,0 +1,19 @@
+/* even and odd number checker */
+
+var num = 9;
+
+/* if (num % 2 === 0){
+    console.log(`${num} is an Even number`)
+}else{
+
+    console.log(`${num } is not an even number` )
+}
+ */
+num % 2 === 0 ? console.log(`${num} is an Even number`) : console.log(`${num} is not an even number` );
+
+
+
+var num = 4.56;
+
+var let = Math.round(num);
+console.log(let)
